51b8032c0fb9b1996a850c065b63f264bf7aff57
Ogenyi25 <johnogenyi4real@gmail.com>
2020-01-18 23:10:56 +0100

    added a collapse components to the  hire me button on the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,17 +8,30 @@ const Navbar = () => {
         <div className="navbar-brand col-md-4">Ogenyi</div>
         <div className="float-right col-md-6">
           <span className="call">Call: 0813 475 6623; 0908 104 6069</span>
-          <button className="book-button" type="button" data-toggle="collapse" data-target="#collapseExample"
+          <button className="book-button" type="button" data-toggle="collapse" data-target="#collapseExercise"
             aria-expanded="false" aria-controls="collapseExample">HIRE ME</button>
-           <button className="book-button" type="button" data-toggle="collapse" data-target="#collapseExample"
+          <button className="book-button" type="button" data-toggle="collapse" data-target="#collapseExample"
             aria-expanded="false" aria-controls="collapseExample">SEE MY WORK</button>
         </div>
       </nav>
-        <div className="collapse" id="collapseExample">
-          <div className="card card-body">
-            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+      <div className="collapse" id="collapseExercise">
+        <div className="card card-body">
+          <form className="card">
+            <input className="form-input" type="text" placeholder="Fullname" />
+            <input className="form-input" type="email" placeholder="Email" />
+          </form>
+        </div>
+      </div>
+
+      <div className="collapse" id="collapseExample">
+        <div className="card card-body">
+          <div className="row">
+            <div className="col-md-4">HOME</div>
+            <div className="col-md-4">CHURCH</div>
+            <div className="col-md-4">SCHOOL</div>
           </div>
         </div>
+      </div>
     </div>
   );
 };
